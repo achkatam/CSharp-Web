f278fd8ab11dbebb096bc968e97e266ef125ceae
achkatam <agmatevv@gmail.com>
2023-05-26 13:16:04 -0400

    added very simple chat app, no database. The messages are saved as variable, just testing the pattern

---
+++ MVC Intro Lab/ChatApp/Controllers/ChatController.cs
@@ -0,0 +1,46 @@
+﻿namespace ChatApp.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+using ViewModels;
+
+public class ChatController : Controller
+{
+    /// <summary>
+    /// Store the messages into a list of KVP..BAD IDEA
+    /// Don't wanna use DB for so simple app
+    /// </summary>
+    private static List<KeyValuePair<string, string>> _messages =
+        new List<KeyValuePair<string, string>>();
+
+    [HttpGet]
+    public IActionResult Show()
+    {
+        if (!_messages.Any())
+        {
+            return View(new ChatViewModel());
+        }
+
+        var chatModel = new ChatViewModel()
+        {
+            Messages = _messages
+                .Select(m => new MessageViewModel()
+                {
+                    Sender = m.Key,
+                    MessageText = m.Value
+                })
+                .ToList()
+        };
+
+        return View(chatModel);
+    }
+
+    [HttpPost]
+    public IActionResult Send(ChatViewModel chat)
+    {
+        var newMessage = chat.CurrentMessage;
+
+        _messages.Add(new KeyValuePair<string, string>(newMessage.Sender, newMessage.MessageText));
+
+        return RedirectToAction("Show");
+    }
+}
---
+++ MVC Intro Lab/ChatApp/ViewModels/ChatViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace ChatApp.ViewModels;
+
+public class ChatViewModel
+{
+    public MessageViewModel CurrentMessage { get; set; } = null!;
+    public List<MessageViewModel> Messages { get; set; } = null!;
+}
